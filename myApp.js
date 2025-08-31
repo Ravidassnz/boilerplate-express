26ff1a57bd4dd2b9658efcd67cd9f3777f02f979
ravidassnz <ravidassnz@gmail.com>
2025-08-31 15:22:57 +1200

    updated myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  const absolutePath = __dirname + '/views/index.html'
+
+  res.send(absolutePath)
 })
 
 console.log(app)
